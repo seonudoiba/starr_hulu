4e26eb4e23dfeeb0e6297818259f9f2e8a2d827d
abiodunseo <biodunrahman1@gmail.com>
2023-04-20 01:11:41 +0100

    Build

--- pages/Favourites.js
+++ pages/Favourites.js
@@ -3,7 +3,7 @@ const Favourites = () => {
     let favorites = ['starr']
   return (
     <div>
-        {favorites.map((favorite,index)=> <div key={index}>{favorite}</div>)}
+        {favorites.map((favorite, index)=> <div key={index} >{favorite}</div>)}
     </div>
   )
 }
